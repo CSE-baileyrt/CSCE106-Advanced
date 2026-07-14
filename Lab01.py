a250071e08722e3a26c4f66970f0b45c50532b99
CSE-baileyrt <143022321+CSE-baileyrt@users.noreply.github.com>
2026-07-14 10:27:34 -0400

    lab 1

---
+++ Lab01.py
@@ -0,0 +1,34 @@
+# The regular assignment for this lab is to complete the FIZZBUZZ challenge
+# https://www.geeksforgeeks.org/dsa/fizz-buzz-implementation/
+
+# The underlying purpose of the assignment is to demonstrate mastery of a while loop, 
+#   conditional logic, and simple arithmetic (the mod operator)
+# Your job is to solve a more advanced version of the FIZZBUZZ problem
+
+##### STEP 1 #####
+# Using a WHILE LOOP (not a for loop), solve the FIZZBUZZ problem as outlined in the link above.
+# NOTE: clarification: print one entry per line (not all appended to form one line of text).
+# NOTE: do not use the "Python" tab as a reference. Do not use AI tools. Write the solution yourself.
+#       If you cannot solve the FIZZBUZZ problem without help, you may not complete the Advanced track.
+#       Email me and resume attending class with everyone else.
+
+##### STEP 2 #####
+# Remove all print statements (do not print the normal "1 2 FIZZ 4 BUZZ" output)
+# Instead, print the following points of data:
+# 1. How many elements match the FIZZ case?
+# 2. What is the sum of all FIZZ elements? (FIZZ only, not including FIZZBUZZ)
+# 3. How many elements match the BUZZ case?
+# 4. What is the sum of all BUZZ elements? (Exclusively BUZZ)
+# 5. How many elements match the FIZZBUZZ case?
+# 6. What is the sum of all FIZZBUZZ elements? (Exclusively FIZZBUZZ)
+# 7. How many elements match none of those cases?
+# NOTE: You will need to make use of variables to keep track of these points
+
+# Display only those 7 outputs, not the normal FIZZBUZZ output.
+# Make each of the 7 lines of output be user friendly.
+# Example output:
+# There were 99 elements that matched the FIZZ case
+# The sum of those 99 elements is: 5
+# There were 101 elements that matched the BUZZ case
+# The sum of those 101 elements is: 10
+# etc...
