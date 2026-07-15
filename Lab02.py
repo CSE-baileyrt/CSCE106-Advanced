4fd36d72986d01c564cedc0ca281585cf204cafd
CSE-baileyrt <143022321+CSE-baileyrt@users.noreply.github.com>
2026-07-15 11:06:29 -0400

    Lab 2

---
+++ Lab02.py
@@ -0,0 +1,20 @@
+# The purpose of this lab is to demonstrate mastery of creating functions
+# NOTE: this assignment is identical to the one the rest of the class will do
+
+### FUNCTIONS ###
+# NOTE: do NOT print the answer inside the function, instead "return" the answer
+# Create a function to calculate the area of a circle (arg: radius)
+#       area of a circle = pi * radius^2 (squared)
+
+# Create a function to calculate the area of a rectangle (args: length, width)
+#       area of a rectangle = length * width
+
+# Create a function to calculate the area of a triangle (args: base, height)
+#       area of a triangle = 1/2 base * height
+
+
+### USER INTERACTION ###
+# 1. Prompt the user for which shape they want
+# 2. Prompt the user for the parameter(s) needed (depending on the shape chosen)
+# 3. Call the appropriate function to fetch the calculation, print the answer
+# Loop steps 1-3 until the user wants to quit
