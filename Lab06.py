83ea542d385dae07e165a6eff97f32cd28070f3b
CSE-baileyrt <143022321+CSE-baileyrt@users.noreply.github.com>
2026-07-15 16:01:17 -0400

    Lab 6

---
+++ Lab06.py
@@ -0,0 +1,22 @@
+# The purpose of this lab is to demonstrate mastery of debugging and exception handling
+
+# Step 1: write flawed code
+# Start by creating code that MIGHT have errors
+#   Examples: dividing by zero, type conversion, IO issues
+#   Your code must have at least 4 things that can go wrong
+#       4+ distinct error types, not 1 problem type that is repeated 4+ times
+#   Your code must be at least 5 lines long and must be a valid program
+#       Runnable, functional, useful, does not have to be fancy
+# Add comments detailing what errors might happen and why
+#   What inputs lead to the errors? What circumstances?
+# Comment out the flawed code so it does not execute
+
+# Step 2: handle errors
+# Make a copy of the code and paste it below the comments you've made so far
+# Alter the copy to properly handle the 4+ issues that might happen
+#   You might use a proactive approach to prevent an error from ever happening
+#   Or you might use a reactive approach to handle an error after it happens
+#   Your solution must include at least one try-except block
+#       The try-except block must contain at least 2 except handlers
+#       The "finally" block is optional
+# Add comments detailing how you have addressed each of the 4+ issues
